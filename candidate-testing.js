cac5b74437820cf0ed1b00d4c88d00d78faf1095
potat <>
2021-05-24 17:29:11 +0000

    returns grade now and second adjustments to variables

--- candidate-testing.js
+++ candidate-testing.js
@@ -10,10 +10,10 @@ let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 let questions = [
   'Who was the first American woman in space? ',
- 'True or false: 5 kilometer == 5000 meters?',
- '(5=3)/2*10 = ? ', 
+ 'True or false: 5 kilometer == 5000 meters? ',
+ '(5 + 3)/2 * 10 = ? ', 
  "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
- 'What is the minimum crew size for the ISS?' 
+ 'What is the minimum crew size for the ISS? ' 
  ];
 let correctAnswers = [
   'Sally Ride',
@@ -79,7 +79,7 @@ function gradeQuiz(candidateAnswers) {
   }
   
     
-
+  return grade;
   
   
  
